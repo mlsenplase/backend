ca873a746a3614e4662127b4cce40dc3caef6a9b
unknown <Administrator@92183276-188815931-55233268.92183276-188815931-55233268>
2026-02-14 03:03:43 -0300

    admin: list users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,4 @@
+import User from "../models/User.js";
 import express from "express";
 import { register, login } from "../controllers/authController.js";
 import { protect, adminOnly } from "../middleware/authMiddleware.js";
@@ -6,6 +7,10 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/users", protect, adminOnly, async (req, res) => {
+  const users = await User.find({}, { senha: 0 }); // remove senha do retorno
+  res.json(users);
+});
 
 router.get("/admin", protect, adminOnly, (req, res) => {
   res.json({ ok: true, message: "Admin OK" });
