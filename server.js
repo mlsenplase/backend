7f64511ef40005b225d40a865f36524075178920
unknown <Administrator@92183276-188815931-55233268.92183276-188815931-55233268>
2026-02-14 00:41:02 -0300

    backend inicial

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo conectado"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
