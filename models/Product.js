3fbe1237807a2461172f97fce1a29d1f60b87265
unknown <Administrator@92183276-188815931-55233268.92183276-188815931-55233268>
2026-02-14 04:24:47 -0300

    fix duplicate productSchema

--- models/Product.js
+++ models/Product.js
@@ -5,21 +5,10 @@ const productSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: String,
     price: { type: Number, required: true },
-    active: { type: Boolean, default: true }
-  },
-  { timestamps: true }
-);
-
-export default mongoose.model("Product", productSchema);
-const productSchema = new mongoose.Schema(
-  image: String,
-  {
-    title: { type: String, required: true },
-    description: String,
-    price: { type: Number, required: true },
     image: String,
     active: { type: Boolean, default: true }
   },
   { timestamps: true }
 );
 
+export default mongoose.model("Product", productSchema);
